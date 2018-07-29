21feb1dffbabf1e76e3d9838d8fc645338ebf234
Mabast Hashm Jamel <mabasthashm1@gmail.com>
2018-07-29 10:45:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(item){
+  drivers.push(item);
+}
+
+function destructivelyPrependDriver(item){
+  drivers.unshift(item);
+}
+
+function  destructivelyRemoveLastDriver(){
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver(){
+  drivers.shift();
+}
+
+function appendDriver(item){
+  const newArr = drivers.push(item);
+}
+
+
+
+
+
+
+
+
+
+
